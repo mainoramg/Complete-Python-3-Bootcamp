e9aa2bf1c962a227d145fb5017b1d84d6d809e8e
mainoramg <mainoramg@gmail.com>
2025-05-27 21:24:01 -0600

    07-Errors and Exception Handling: solutions

--- 07-Errors and Exception Handling/cap.py
+++ 07-Errors and Exception Handling/cap.py
@@ -1,2 +1,5 @@
+import string
+
 def cap_text(text):
-    return text.title()  # replace .capitalize() with .title()
+    # return text.title()  # replace .capitalize() with .title()
+    return string.capwords(text)
